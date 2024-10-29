8b6d473c069a748ae9e0c03333f853b96b0a77a1
Matt Walters <github@iammattwalters.com>
2024-10-29 17:31:03 -0500

    feat: Implement basic Git plugin with clone command functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,38 @@
+// #[no_mangle]
+// pub extern "C" fn register_plugin() {
+//     println!("meta-git plugin loaded");
+//     // Here you can add more functionality, like registering commands
+// }
+use meta::{Plugin, PluginError};
+
+pub struct GitPlugin;
+
+impl Plugin for GitPlugin {
+    fn name(&self) -> &'static str {
+        "git"
+    }
+
+    fn commands(&self) -> Vec<&'static str> {
+        vec!["git"]
+    }
+
+    fn execute(&self, command: &str, args: &[String]) -> anyhow::Result<()> {
+        match command {
+            "git clone" => {
+                if args.is_empty() {
+                    println!("Usage: meta git clone <repository>");
+                    return Ok(());
+                }
+                // Implement git clone functionality
+                println!("Cloning repository: {}", args[0]);
+                Ok(())
+            }
+            _ => Err(PluginError::CommandNotFound(command.to_string()).into()),
+        }
+    }
+}
+
 #[no_mangle]
-pub extern "C" fn register_plugin() {
-    println!("meta-git plugin loaded");
-    // Here you can add more functionality, like registering commands
+pub extern "C" fn _plugin_create() -> *mut dyn Plugin {
+    Box::into_raw(Box::new(GitPlugin))
 }
